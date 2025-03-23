31943514a2e678dc3ec74828ee06a3146bebecdf
Ross Llewallyn <rossllewallyn@proton.me>
2025-03-23 00:42:12 -0400

    typo and more UI

--- ui/src/App.js
+++ ui/src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import React, { useState, useEffect } from "react";
 
 function App() {
-  const [data, setData] = useState({
+  const [data, setdata] = useState({
     id: 0,
     name: "",
     type: "",
@@ -26,7 +26,11 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h1>Testing...</h1>
+        {}
+        <p>{data.id}</p>
         <p>{data.name}</p>
+        <p>{data.type}</p>
       </header>
     </div>
   );
